8563b9f5f078cd99b9a9cb026ac958b87e28f51c
mulatya <emmanuelmulatya8@gmail.com>
2017-07-14 16:32:20 +0300

    added update property

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+   update(Question, params) {
+     this.sendAction('update', Question, params);
+   },
+   }
+
 });
