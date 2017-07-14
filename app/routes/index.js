d22be66c62c51ee482cd6538acd389d73494cd3e
mulatya <emmanuelmulatya8@gmail.com>
2017-07-14 12:52:12 +0300

    put my question index model

---
+++ app/routes/index.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+var questions = [{
+  id: 1,
+  author: "Sugar spicy",
+  question: "lodid bbvw bwunvw buiwv?",
+}, {
+  id: 2,
+  author: "Mike TV",
+  questio: "nreinbi JNION d?",
+
+}, {
+  id: 3,
+  author: "Red May",
+  questio: "Why do I love staring at fish so much?",
+
+}];
+
+export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
+});
